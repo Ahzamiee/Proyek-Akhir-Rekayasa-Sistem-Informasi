c78fdba9209186ba47d7440e27f241f0a570b4b0
Ahzamiee <dzikhzami0306@gmail.com>
2025-12-18 17:37:14 +0700

    Add files via upload

---
+++ app/Http/Controllers/Event/CariEventController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Event;
+
+use App\Http\Controllers\Controller;
+use App\Models\Event;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CariEventController extends Controller
+{
+    public function search(Request $request)
+    {
+        $search = $request->search;
+
+        $events = Event::where('title', 'like', "%$search%")
+            ->orWhere('location', 'like', "%$search%")
+            ->orWhere('city', 'like', "%$search%")
+            ->get();
+
+
+        $kotaList = [
+            'Jakarta','Surabaya','Bandung','Medan','Depok','Makassar',
+            'Semarang','Bali','Batam','Yogyakarta'
+        ];
+
+        return view('event.index', [
+            'events' => $events,
+            'kotaList' => $kotaList,
+            'search' => $search,
+            'selectedCategory' => null,
+            'selectedCities' => []
+        ]);
+    }
+}
